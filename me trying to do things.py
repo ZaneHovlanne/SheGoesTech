37d9e8c6830355bd700f2b94a3736916b8fe5bc4
Zane Hovlanne <112967814+ZaneHovlanne@users.noreply.github.com>
2022-09-12 20:46:30 +0300

    Add files via upload

---
+++ me trying to do things.py
@@ -0,0 +1,67 @@
+# age = 20
+# first_name = "John"
+# surname = "Smith"
+# status = "New patient"
+# print (first_name, surname, age, status)
+# name = input ("What is your name? ")
+# print("Hello " + name )
+
+# birth_year = input("Enter your birth year: ")
+# age = 2022 - int(birth_year)
+# print(age)
+
+# first_value = input ("First: ")
+# second_value = input ("Second: ")
+# together = float(first_value) + float(second_value)
+# print("Sum: " + str(together))
+# OR
+# first_value = float(input ("First: "))
+# second_value = float(input ("Second: "))
+# together = first_value + second_value
+# print("Sum: " + str(together))
+
+# course = "Python for Beginners"
+# print(course.replace("for", "4"))
+
+# x = 10 + 3 * 2
+# print (x)
+
+# x = 3 > 2
+# print (x)
+# > - bigger than
+# == -equal
+#!= - notequal
+# >= bigger than or equal
+
+# price = 5
+# print(not price > 10)
+
+#if used
+# and (both needs to be true to be true)
+# or (at least one needs to be true to return True)
+# not (reverses any given value)
+
+# temperature = 11
+# if temperature > 30: 
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif temperature >20:
+#     print("It's a nice day")
+# elif temperature >10:
+#     print ("It's a bit cold")
+# else:7
+#     print ("It's cold")
+# print ("Done")
+
+# weight = int(input("Weight: "))
+# unit = input("(K)g or (L)bs: ")
+# if unit.upper() == "K":
+#     converted = weight / 0.45
+#     print("Weight in Lbs: " + str(converted))
+# else:
+#     converted = weight * 0.45
+#     print("Weight in Kgs: " + str(converted))
+# print("Done")
+
+
+
