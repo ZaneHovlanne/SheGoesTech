d5c13ee28f66ce05f92c5b8dc70ad2893014d0b0
Zane Hovlanne <112967814+ZaneHovlanne@users.noreply.github.com>
2022-09-12 22:05:42 +0300

    Add files via upload

--- 12.09_exercises.py
+++ 12.09_exercises.py
@@ -1,18 +1,20 @@
 # Exercise 1
-temperature = float(input("What is your temperature today? "))
-if temperature < 35:
-    print("Not too cold")
-elif  temperature > 37:
-    print("Posible fever! Buddy, get yourself in bed with tea!")
-else:
-    print("All right!")
+# temperature = float(input("What is your temperature today? "))
+# if temperature < 35:
+#     print("Not too cold")
+# elif  temperature > 37:
+#     print("Posible fever! Buddy, get yourself in bed with tea!")
+# else:
+#     print("All right!")
 
-#Exercise2
+# #Exercise2
 
 monthly_salary = float(input("What is your montly salary? "))
 years_worked = float(input("How many years have you worked here? "))
 bonus_rate = 0.15
+bonus_treshold = 2
 if years_worked < 2:
     print("Your bonus amount is 0! Don't be sad, your bonus is coming next year, keep up the good work!")
 else:
-    print("Your bonus comes up to " + str(monthly_salary * bonus_rate * years_worked))
+    bonus = str(round((years_worked - bonus_treshold) * monthly_salary * bonus_rate))
+    print("Your bonus comes up to " + bonus)
