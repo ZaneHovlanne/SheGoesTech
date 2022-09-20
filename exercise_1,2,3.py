1d95e49214a73cfd3e47f63ce079a9b7f7bf4c63
Zane Hovlanne <112967814+ZaneHovlanne@users.noreply.github.com>
2022-09-20 22:11:33 +0300

    Add files via upload

--- exercise_1,2,3.py
+++ exercise_1,2,3.py
@@ -1,37 +1,35 @@
-# 1c
+# # 1c
 
-list_numbers = []
+# list_numbers = []
 
+# while True:
+#     user_input = input(
+#         "Please enter a number or 'q' to quit: ")
+#     if user_input == "q":
+#         print("You quit")
+#         break
 
-while True:
-    numbers_entered = input(
-        "Please enter numbers(separated by comma) or 'q' to quit: ")
-    if numbers_entered == "q":
-        print("You quit")
-        break
-
-    else:
-        list_numbers.append(float(numbers_entered))
-        sorted_number_list = sorted(numbers_entered)
-        average_calculated = sum(numbers_entered) / len(numbers_entered)
-        print(
-            f"Numbers you enetered are: {numbers_entered} , average is: {average_calculated}")
-
-        print(f"Top 3 values you enetered are: {sorted_number_list[-3:]}")
-        print(f"Bottom 3 values you entered are: {sorted_number_list[:3]}")
+#     else:
+#         list_numbers.append(float(user_input))
+#         sorted_number_list = sorted(list_numbers)
+#         average_calculated = sum(list_numbers) / len(list_numbers)
+#         print(
+#             f"Numbers you enetered are: {list_numbers} , average is: {average_calculated}")
+#         print(f"Top 3 values you enetered are: {sorted_number_list[-3:]}")
+#         print(f"Bottom 3 values you entered are: {sorted_number_list[:3]}")
 
 
-# exercise 2
-start_integer = int(input("Please enter the beggining integer: "))
-end_integer = int(input("Please enter the end integer: "))
-numbers = list(range(start_integer, end_integer))
-squares = []
-for num in numbers:
-    squares.append(num**2)
-    print(f"{num} cubed {num**2}")
-print(squares)
+# # exercise 2
+# start_integer = int(input("Please enter the beggining integer: "))
+# end_integer = int(input("Please enter the end integer: "))
+# numbers = list(range(start_integer, end_integer+1))
+# squares = []
+# for num in numbers:
+#     squares.append(num**2)
+#     print(f"{num} cubed {num**2}")
+# print(squares)
 
-# task No 3
+# # task No 3
 sentence_entered = input("Please enter a sentence: ")
 
 split_sentence = sentence_entered.split()
