836ebfe5a6c5752baad6a8648c9d435ccb72da11
Zane Hovlanne <112967814+ZaneHovlanne@users.noreply.github.com>
2022-09-27 20:44:45 +0300

    Add files via upload

---
+++ 2nd_task_a.py
@@ -0,0 +1,10 @@
+def get_common_elements(seq1, seq2, seq3):
+    seq1_set = set(seq1)
+    seq2_set = set(seq2)
+    seq3_set = set(seq3)
+    result = set(seq1_set & seq2_set & seq3_set)
+    tuple_result = tuple(result)
+    return tuple_result
+
+
+print(get_common_elements("abc", ['a', 'b'], ('b', 'd')))
